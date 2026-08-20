34f273d8d77149bbf1aa4b374d7362d53d42fbf0
js-louiss <js.curiqueor@gmail.com>
2026-08-19 23:48:32 -0400

    Add: method to return to the previous window, and go to window

--- src/Helpers/Navigation.java
+++ src/Helpers/Navigation.java
@@ -5,13 +5,30 @@
 package Helpers;
 
 import java.awt.Component;
+import java.awt.Window;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 public class Navigation {
+
     public static void Salir(Component parent) {
         int Opcion = JOptionPane.showConfirmDialog(parent, "Esta seguro de salir?", "Salir", 0);
         if (Opcion == 0) {
             System.exit(0);
         }
-    } 
+    }
+
+    public static void Volver(Component parent, JFrame anterior) {
+        int Opcion = JOptionPane.showConfirmDialog(parent, "Esta seguro de volver?", "Salir", JOptionPane.YES_NO_OPTION);
+        if (Opcion == JOptionPane.YES_OPTION) {
+            Window actual = SwingUtilities.getWindowAncestor(parent);
+
+            if (actual != null) {
+                actual.dispose();
+            }
+
+            anterior.setVisible(true);
+        }
+    }
 }
